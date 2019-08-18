df436b7cba488a074095180d6939646fa089540b
Manish Kumar Sadhu <sadhumanishkumar@gmail.com>
2019-08-18 22:30:49 +0530

    Chore : remove duplicate code

--- src/app/core/CustomMaterialModule .ts
+++ src/app/core/CustomMaterialModule .ts
@@ -1,37 +1,26 @@
 import {NgModule} from "@angular/core";
-import { CommonModule } from '@angular/common';
-
-
 import  {MatButtonModule, MatCardModule, MatMenuModule ,  MatInputModule, MatCheckboxModule,
     MatListModule, MatToolbarModule, MatSidenavModule, MatIconModule  ,MatProgressSpinnerModule} from '@angular/material';
 
+    const MaterialComponents = [
+      MatToolbarModule,
+      MatButtonModule,
+      MatListModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatMenuModule,
+      MatCardModule,
+      MatInputModule,
+      MatProgressSpinnerModule,
+      MatCheckboxModule
+    ]
 
     @NgModule({
         imports: [
-          CommonModule,
-          MatToolbarModule,
-          MatButtonModule,
-          MatListModule,
-          MatSidenavModule,
-          MatIconModule,
-          MatMenuModule,
-          MatCardModule,
-          MatInputModule,
-          MatProgressSpinnerModule,
-          MatCheckboxModule
+          MaterialComponents
         ],
         exports:[
-            CommonModule,
-          MatToolbarModule,
-          MatButtonModule,
-          MatListModule,
-          MatSidenavModule,
-          MatIconModule,
-          MatMenuModule,
-          MatCardModule,
-          MatInputModule,
-          MatProgressSpinnerModule,
-          MatCheckboxModule
+          MaterialComponents
         ]
       })
 
