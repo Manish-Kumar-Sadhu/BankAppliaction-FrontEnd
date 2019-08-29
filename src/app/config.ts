4a60ddb1ebb218ee37f1037af79de090cb62403b
Manish Kumar Sadhu <sadhumanishkumar@gmail.com>
2019-08-29 13:07:22 +0530

    fix: removed unused files

--- src/app/config.ts
+++
@@ -1,3 +0,0 @@
-export class config {
-    public static BASE_URL = 'http://localhost:8080';
-}
--- src/app/customer.ts
+++
@@ -1,5 +0,0 @@
-export class Customer {
- constructor(public id: number,public name: string,public mobile: number,public email:string,public state:string,public number_of_accounts:number){
-
- }
-}
